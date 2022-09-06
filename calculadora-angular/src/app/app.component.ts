94ab74d11e1eac688e5ad9fe644da3979250e37b
IzanDuesoLopez <izandueso@gmail.com>
2022-09-06 13:06:39 +0200

    Calculadora Angular - TA34

---
+++ calculadora-angular/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { last } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculadora-angular';
+  num:any = "";
+  numAnterior:any = "";
+  historico:any = "";
+  resultado:number = 0;
+  lastButton:string = "";
+
+  addnum(num:string){
+    this.num += num;
+  }
+
+  suma(){
+    this.numAnterior = this.num;
+    this.num = "";
+    this.resultado += parseInt(this.numAnterior);
+    this.historico += this.numAnterior + " + ";
+    this.lastButton = "suma";
+  }
+
+  igual(){
+    switch(this.lastButton){
+      case "suma":
+        this.historico += this.num + " = ";
+        this.resultado += parseInt(this.num);
+        break;
+    }
+    this.num = this.resultado;
+  }
+
+  borrar(){
+    this.num = "";
+    this.historico = "";
+    this.numAnterior = "";
+    this.resultado = 0;
+    this.lastButton = "";
+  }
+
+}
